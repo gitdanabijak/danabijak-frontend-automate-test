28a6e74faef171636f0789197e7349b5a84677e1
Lihar Sendhi Wijaya <liharsw@gmail.com>
2022-07-28 16:48:15 +0700

    added: login page

---
+++ features/login.feature
@@ -0,0 +1,8 @@
+Feature: Homepage presence
+
+    Scenario: User can login
+        Given open browser
+        When open danabijak login site
+        Then user input email
+        And user input password
+        And user click login
---
+++ features/steps/login.py
@@ -0,0 +1,25 @@
+from behave import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@when('open danabijak login site')
+def homepage_show_up(context):
+    context.driver.get('https://danabijak.com/login')
+
+@then('user input email')
+def user_input_email(context):
+    element = context.driver.find_element(By.NAME, 'login')
+    element.send_keys("ibnutest3@danabijak.com")
+
+@then('user input password')
+def user_input_password(context):
+    element = context.driver.find_element(By.NAME, 'password')
+    element.send_keys("230998")
+    
+@then('user click login')
+def user_click_login(context):
+    context.driver.find_element(By.XPATH, "//button[@type='commit']").click()
+    WebDriverWait(context.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[normalize-space()='Selamat datang, Ibnu Testing!']")))
+
